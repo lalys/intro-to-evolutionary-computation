ab37c484b340f2694f9455c53e94e48bd14386a7
lalys <xion1337@morris.umn.edu>
2019-09-16 15:30:54 -0500

    Create heuristic_search.clj

---
+++ intro-to-ec/src/intro_to_ec/heuristic_search.clj
@@ -0,0 +1,47 @@
+(ns intro-to-ec.heuristic_search
+  (:require [clojure.set :as cset]))
+
+(defn remove-previous-states
+  [new-states frontier visited]
+  (remove (cset/union (set frontier) (set visited)) new-states))
+
+(def depth-first-search
+  {:get-next-node first
+   :add-children concat})
+
+(def breadth-first-search
+  {:get-next-node first
+   :add-children #(concat %2 %1)})
+
+(def random-search
+  {:get-next-node rand-nth
+   :add-children concat})
+
+(defn generate-path
+  [came-from node]
+  (if (= :start-node (get came-from node))
+    [node]
+    (conj (generate-path came-from (get came-from node)) node)))
+
+(defn search
+  [{:keys [get-next-node add-children]}
+   {:keys [goal? make-children]}
+   start-node max-calls]
+  (loop [frontier [start-node]
+         came-from {start-node :start-node}
+         num-calls 0]
+    (println num-calls ": " frontier)
+    (println came-from)
+    (let [current-node (get-next-node frontier)]
+      (cond
+        (goal? current-node) (generate-path came-from current-node)
+        (= num-calls max-calls) :max-calls-reached
+        :else
+        (let [kids (remove-previous-states
+                    (make-children current-node) frontier (keys came-from))]
+          (recur
+           (add-children
+            kids
+            (rest frontier))
+           (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
+           (inc num-calls)))))))
