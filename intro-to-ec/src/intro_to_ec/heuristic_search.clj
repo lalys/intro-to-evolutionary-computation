06de3d9f64dc2ccaaeab708f708cdd7cf82d6ff7
lalys <xion1337@morris.umn.edu>
2019-09-25 22:08:01 -0500

    Implement heuristic search Add hearustic to heuristic-search Make frontier a priority-map
    Co-Authored-By: null <welch298@users.noreply.github.com>

--- intro-to-ec/src/intro_to_ec/heuristic_search.clj
+++ intro-to-ec/src/intro_to_ec/heuristic_search.clj
@@ -2,8 +2,11 @@
   (:require [clojure.set :as cset]
             [clojure.data.priority-map :as cpm]))
 
-;;(require '[clojure.data.priority-map :as cpm])
-;;use cpm/priority-map to call priority-map
+;(require '[clojure.data.priority-map :as cpm])
+;use cpm/priority-map to call priority-map
+;(require '[intro-to-ec.heuristic_search :as hs])
+;(require '[intro-to-ec.grid-problem-with-walls :as gpw])
+;(hs/search hs/breadth-first-search (gpw/make-grid-problem -10 10 gpw/low-vertical-wall)[4 4] 100)
 
 (defn remove-previous-states
   [new-states frontier visited]
@@ -27,20 +30,9 @@
     [node]
     (conj (generate-path came-from (get came-from node)) node)))
 
-(defn children
-  {reduce (fn [cf child] (assoc cf child current-node)) came-from kids
-    })
-
-(defn hs
-  {:get-next-node first
-  :add-children children})
-
-
-(defn abs [n] (max n (- n)))
-
-(def h-search
-  {
-   return abs(a.x - b.x) + abs(a.y - b.y)})
+(def hs
+  {:get-next-node #(first (first %))
+  :add-children #(reduce (fn [front child] (assoc front child (%1 child))) )})
 
   (defn search
     [{:keys [get-next-node add-children]}
@@ -67,9 +59,9 @@
 
 (defn heuristic-search
   [{:keys [get-next-node add-children]}
-   {:keys [goal? make-children]}
+   {:keys [goal? make-children heuristic]}
    start-node max-calls]
-  (loop [frontier [:a :b :c] ;; Make priority map
+  (loop [frontier (cpm/priority-map first (heuristic first)) ;; Make priority map
          came-from {start-node :start-node}
          num-calls 0]
     (println num-calls ": " frontier)
@@ -83,7 +75,9 @@
                     (make-children current-node) frontier (keys came-from))]
           (recur
            (add-children
+            heuristic
+            (pop frontier)
             kids
             (rest frontier))
-           (reduce (fn [cf child] (assoc cf child current-node)) came-from kids) ;; Add heuristic-search
+           (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
            (inc num-calls)))))))
