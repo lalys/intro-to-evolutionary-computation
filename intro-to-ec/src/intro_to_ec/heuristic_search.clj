fdf47e97beccca0bcecb5d2e5c766e97c55229e3
lalys <xion1337@morris.umn.edu>
2019-09-28 20:42:49 -0500

    Make heuristic search function work
    Add comments on how to set up search
    Make A star search
    
    Co-Authored-By: null <welch298@users.noreply.github.com>

--- intro-to-ec/src/intro_to_ec/heuristic_search.clj
+++ intro-to-ec/src/intro_to_ec/heuristic_search.clj
@@ -1,12 +1,14 @@
 (ns intro-to-ec.heuristic_search
   (:require [clojure.set :as cset]
-            [clojure.data.priority-map :as cpm]))
+            [clojure.data.priority-map :as cpm]
+            ;[intro-to-ec.grid-problem-with-walls :as gpw]
+            ;[intro-to-ec.heuristic_search :as hs]
+            ))
 
-;(require '[clojure.data.priority-map :as cpm])
+;(require '[clojure.data.priority-map :as cpm]) (require '[intro-to-ec.heuristic_search :as hs]) (require '[intro-to-ec.grid-problem-with-walls :as gpw])
 ;use cpm/priority-map to call priority-map
-;(require '[intro-to-ec.heuristic_search :as hs])
-;(require '[intro-to-ec.grid-problem-with-walls :as gpw])
 ;(hs/search hs/breadth-first-search (gpw/make-grid-problem -10 10 gpw/low-vertical-wall)[4 4] 100)
+;(hs/heuristic-search hs/heu (gpw/make-grid-problem -10 10 gpw/low-vertical-wall)[4 4] 50)
 
 (defn remove-previous-states
   [new-states frontier visited]
@@ -30,9 +32,9 @@
     [node]
     (conj (generate-path came-from (get came-from node)) node)))
 
-(def hs
+(def heu
   {:get-next-node #(first (first %))
-  :add-children #(reduce (fn [front child] (assoc front child (%1 child))) )})
+  :add-children #(reduce (fn [first child] (assoc first child (%1 child nil nil))) %2 %3)})
 
   (defn search
     [{:keys [get-next-node add-children]}
@@ -61,7 +63,7 @@
   [{:keys [get-next-node add-children]}
    {:keys [goal? make-children heuristic]}
    start-node max-calls]
-  (loop [frontier (cpm/priority-map first (heuristic first)) ;; Make priority map
+  (loop [frontier (cpm/priority-map start-node (heuristic start-node nil nil))
          came-from {start-node :start-node}
          num-calls 0]
     (println num-calls ": " frontier)
@@ -78,6 +80,29 @@
             heuristic
             (pop frontier)
             kids
-            (rest frontier))
+            )
            (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
            (inc num-calls)))))))
+
+ (defn A-star-search
+  [{:keys [get-next-node add-children]}
+   {:keys [goal? make-children heuristic]}
+   start-node max-calls]
+  (loop [frontier [start-node]
+         came-from {start-node :start-node}
+         num-calls 0]
+    (println num-calls ": " frontier)
+    (println came-from)
+    (let [current-node (get-next-node frontier)]
+      (cond
+        (goal? current-node) (generate-path came-from current-node)
+        (= num-calls max-calls) :max-calls-reached
+        :else
+        (let [kids (remove-previous-states
+                    (make-children current-node) frontier (keys came-from))]
+          (recur
+           (add-children
+            kids
+            (rest frontier))
+          (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
+          (inc num-calls)))))))
