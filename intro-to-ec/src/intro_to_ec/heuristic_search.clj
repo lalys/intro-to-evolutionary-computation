b4396a5cd535904c98b033c54181e322e3e9e11f
lalys <xion1337@morris.umn.edu>
2019-10-09 15:36:27 -0500

    Add a-star heauristic
    Add A-star-search
    Add cost-so-far and new-cost into A-star-search

--- intro-to-ec/src/intro_to_ec/heuristic_search.clj
+++ intro-to-ec/src/intro_to_ec/heuristic_search.clj
@@ -9,6 +9,7 @@
 ;use cpm/priority-map to call priority-map
 ;(hs/search hs/breadth-first-search (gpw/make-grid-problem -10 10 gpw/low-vertical-wall)[4 4] 100)
 ;(hs/heuristic-search hs/heu (gpw/make-grid-problem -10 10 gpw/low-vertical-wall)[4 4] 50)
+;(hs/A-star-search hs/a-star (gpw/make-grid-problem -10 10 gpw/low-vertical-wall hs/a-star)[4 4] 50)
 
 (defn remove-previous-states
   [new-states frontier visited]
@@ -35,6 +36,11 @@
 (def heu
   {:get-next-node #(first (first %))
   :add-children #(reduce (fn [first child] (assoc first child (%1 child nil nil))) %2 %3)})
+
+
+(def a-star
+  {:get-next-node #(first (first %))
+    :add-children #(reduce (fn [first child] (assoc first child ( + (%1 child) %4 ))) %2 %3)})
 
   (defn search
     [{:keys [get-next-node add-children]}
@@ -76,11 +82,7 @@
         (let [kids (remove-previous-states
                     (make-children current-node) frontier (keys came-from))]
           (recur
-           (add-children
-            heuristic
-            (pop frontier)
-            kids
-            )
+           (add-children heuristic (pop frontier) kids)
            (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
            (inc num-calls)))))))
 
@@ -88,12 +90,15 @@
   [{:keys [get-next-node add-children]}
    {:keys [goal? make-children heuristic]}
    start-node max-calls]
-  (loop [frontier [start-node]
+  (loop [frontier (cpm/priority-map start-node (heuristic start-node) nil nil)
+         cost-so-far {start-node 0}
          came-from {start-node :start-node}
          num-calls 0]
     (println num-calls ": " frontier)
     (println came-from)
-    (let [current-node (get-next-node frontier)]
+    (println "The cost is: " cost-so-far)
+    (let [current-node (get-next-node frontier)
+          new-cost (+ (get cost-so-far current-node) 1)]
       (cond
         (goal? current-node) (generate-path came-from current-node)
         (= num-calls max-calls) :max-calls-reached
@@ -101,8 +106,7 @@
         (let [kids (remove-previous-states
                     (make-children current-node) frontier (keys came-from))]
           (recur
-           (add-children
-            kids
-            (rest frontier))
+          (add-children heuristic (pop frontier) kids new-cost)
+          (reduce (fn [c child] (assoc c child (heuristic child nil nil))) cost-so-far kids)
           (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
           (inc num-calls)))))))
